96f0e28cf28ef15a3a2e7a2ba7c3f156d785417f
gmtstephane <gmtstephane@gmail.com>
2023-02-28 20:29:46 +0100

    add termination message

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
+	"os"
 
 	"github.com/gmtstephane/kpture/api/capture"
 	"github.com/gmtstephane/kpture/pkg/kpture"
@@ -20,18 +20,19 @@ func main() {
 
 	serverOptions, err := kpture.OptFromEnv()
 	if err != nil {
-		logrus.Error(err)
-		return
+		terminationMessage(err)
+		os.Exit(1)
 	}
 	s, err := pcap.NewCaptureServer(serverOptions...)
 	if err != nil {
-		logrus.Error(err)
-		return
+		terminationMessage(err)
+		os.Exit(1)
 	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", s.Port()))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		terminationMessage(err)
+		os.Exit(1)
 	}
 
 	logrus.Info("starting gRPC server on port ", s.Port())
@@ -41,4 +42,17 @@ func main() {
 	capture.RegisterKptureServer(grpcServer, s)
 	reflection.Register(grpcServer)
 	logrus.Error(grpcServer.Serve(lis))
+}
+
+func terminationMessage(e error) {
+	logrus.Error(e)
+	terminationFile, err := os.OpenFile("/dev/termination-log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o666)
+	if err != nil {
+		logrus.Error(err)
+	}
+	defer terminationFile.Close()
+	_, err = terminationFile.WriteString(e.Error())
+	if err != nil {
+		logrus.Error(err)
+	}
 }
